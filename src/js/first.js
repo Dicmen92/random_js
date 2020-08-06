dfff54b8c3db7071dcc50dd1d88bc6111a60e671
Dmitriy Babich <babich-d@mail.ru>
2020-08-07 00:37:16 +0600

    Выполнили задания по созданию переменных и отдельной ветки lesson01, вывод текста

---
+++ src/js/first.js
@@ -0,0 +1,10 @@
+let money = 10000;
+let income = 4000;
+let addExpenses = 2000;
+let deposit = 4000;
+const mission = 'Сдать первый урок на УРА';
+const period = '06.08.2020 г.';
+
+alert("Привет мир");
+
+console.log('Эту запись мы отображаем в Консоли')
