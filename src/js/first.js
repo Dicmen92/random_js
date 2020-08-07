517de21358cabfddb3d73ee688a7f5c864a0a240
Dmitriy Babich <babich-d@mail.ru>
2020-08-08 01:02:08 +0600

    Создали новую ветку lesson02

--- src/js/first.js
+++ src/js/first.js
@@ -1,9 +1,9 @@
-let money = 10000;
-let income = 4000;
-let addExpenses = 2000;
-let deposit = 4000;
-const mission = 'Сдать первый урок на УРА';
-const period = '06.08.2020 г.';
+let money = 10000,
+income = 4000,
+addExpenses = 2000,
+deposit = 4000;
+const mission = 'Сдать первый урок на УРА',
+period = '06.08.2020 г.';
 
 alert("Привет мир");
 
