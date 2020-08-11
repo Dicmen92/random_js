8f64e77bffe2757007e122368dd326279afd7115
Dmitriy Babich <babich-d@mail.ru>
2020-08-12 01:34:03 +0600

    Сделаны правки: при вводе цифры в строку сообщения, она не попадает в консоль

--- src/js/complicated.js
+++ src/js/complicated.js
@@ -4,15 +4,15 @@ let inpLine = '';
 
 function acceptAnArgument(inpLine) {
   inpLine = prompt('Введите ваше сообщение', 'Например, меня зовут Дмитрий.');
-  if ( !isNaN(inpLine) ) {
-    alert('Извините, но введенное Вами сообщение не должно содержать цифр');  
-  }  
-  inpLine = inpLine.trim();  
+  inpLine = inpLine.trim();
   if (inpLine.length > 30) { 
     inpLine = inpLine.substring(0, 30) + "...";    
 }
-console.log(inpLine); 
-  
+  if ( !isNaN(inpLine) ) {
+    alert('Извините, но введенное Вами сообщение не должно содержать цифр');  
+  } else {
+    console.log(inpLine); 
+  }  
 }
 
 
