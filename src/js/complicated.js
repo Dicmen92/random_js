7afb87960a57e7beb2fe7ee708338a429d482876
Dmitriy Babich <babich-d@mail.ru>
2020-08-13 22:45:17 +0600

    Выполнил задания по усложненному уроку №5

--- src/js/complicated.js
+++ src/js/complicated.js
@@ -1,19 +1,46 @@
 'use strict';
 
-let inpLine = '';
-
-function acceptAnArgument(inpLine) {
-  inpLine = prompt('Введите ваше сообщение', 'Например, меня зовут Дмитрий.');
-  inpLine = inpLine.trim();
-  if (inpLine.length > 30) { 
-    inpLine = inpLine.substring(0, 30) + "...";    
+let isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
-  if ( !isNaN(inpLine) ) {
-    alert('Извините, но введенное Вами сообщение не должно содержать цифр');  
-  } else {
-    console.log(inpLine); 
+
+let digit = 0;
+
+let checkDigit = function() {
+  do {
+    digit = prompt('Введите 7 любых многозначных чисел');
   }  
+  while (!isNumber(digit))
+};
+
+let arr = [];
+
+let outDigit = function() {    
+  for (let i = 0; i < 7; i++) {
+    checkDigit();
+    arr[i] = digit;       
+  }  
+  let n = 1;
+  for (let i = 0; i < 7; i++) {            
+    console.log('это строка №', n, 'и она содержит число = ', arr[i]);       
+    n = n + 1;
+  }  
+};
+
+outDigit();
+
+for (let i = 0; i < 7; i++) {
+if (arr[i][0] == '2' || arr[i][0] == '4') {
+  console.log('Это число начинается с цифры 2 или 4 = ', arr[i]);
 }
+};
 
+let n = 100;
 
-acceptAnArgument(inpLine);
+nextPrime:
+for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime;
+  }
+  console.log(i, 'Делители этого числа: 1 и ', i);
+}
